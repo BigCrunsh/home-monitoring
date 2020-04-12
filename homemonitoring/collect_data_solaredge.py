6f1745139acb3e8a7ee627f258404146d46a0dda
Christoph Sawade <christoph@sawade.me>
2020-04-12 20:42:59 +0200

    Change order of arguments
    end_date can be optional in this context time zone not.

--- homemonitoring/collect_data_solaredge.py
+++ homemonitoring/collect_data_solaredge.py
@@ -3,32 +3,12 @@
 import os
 import argparse
 import sys
-import datetime
-import pytz
 
-import pandas as pd
-import numpy as np
 import solaredge
 from influxdb import InfluxDBClient
 
 from homemonitoring.response_mappers import SolarEdgeResponseMapper
-from homemonitoring.util import get_latest_timestamp_influxdb, LoggerConfig
-
-
-def get_date_ranges(start_datetime, time_zone):
-    tz = pytz.timezone(time_zone)
-    end_datetime = datetime.datetime.now(tz).replace(microsecond=0)
-    # round to next 15 mins
-    start_datetime = tz.localize(start_datetime)
-    start_datetime += datetime.timedelta(minutes=15 - start_datetime.minute % 15)
-    start_datetime -= datetime.timedelta(seconds=start_datetime.second)
-
-    end_month_dates = pd.date_range(
-        start_datetime, end_datetime, freq='M', normalize=True
-    ).to_pydatetime()
-    end_dates = np.append(end_month_dates, end_datetime)
-    start_dates = np.append(start_datetime, end_dates+datetime.timedelta(days=1))
-    return start_dates, end_dates
+from homemonitoring.util import get_latest_timestamp_influxdb, get_date_ranges, LoggerConfig
 
 
 def run(args):
@@ -48,14 +28,18 @@ def run(args):
     measurement_name = 'electricity_power_watt'
     logger.info(f'{measurement_name}')
     start_dates, end_dates = get_date_ranges(
+        meta['location']['timeZone'],
         get_latest_timestamp_influxdb(measurement_name, ifclient, meta['installationDate']),
-        meta['location']['timeZone']
     )
     logger.info(f'collect data for {start_dates[0]}-{end_dates[-1]}')
     num_points = 0
     for s, e in zip(start_dates, end_dates):
-        response = api.get_power_details(site_id=meta['id'], start_time=s.replace(tzinfo=None), end_time=e.replace(tzinfo=None))
-        points = SolarEdgeResponseMapper.to_influxdb_point(response, meta['location']['timeZone'], measurement_name)
+        response = api.get_power_details(
+            site_id=meta['id'], start_time=s.replace(tzinfo=None), end_time=e.replace(tzinfo=None)
+        )
+        points = SolarEdgeResponseMapper.to_influxdb_point(
+            response, meta['location']['timeZone'], measurement_name
+        )
         ifclient.write_points(points)
         num_points += len(points)
     logger.info(f'data points added: {num_points}')
@@ -70,9 +54,12 @@ def run(args):
     num_points = 0
     for s, e in zip(start_dates, end_dates):
         response = api.get_energy_details(
-            site_id=meta['id'], start_time=s.replace(tzinfo=None), end_time=e.replace(tzinfo=None), time_unit='QUARTER_OF_AN_HOUR'
+            site_id=meta['id'], start_time=s.replace(tzinfo=None), end_time=e.replace(tzinfo=None),
+            time_unit='QUARTER_OF_AN_HOUR'
         )
-        points = SolarEdgeResponseMapper.to_influxdb_point(response, meta['location']['timeZone'], measurement_name)
+        points = SolarEdgeResponseMapper.to_influxdb_point(
+            response, meta['location']['timeZone'], measurement_name
+        )
         ifclient.write_points(points)
         num_points += len(points)
     logger.info(f'data points added: {num_points}')
--- homemonitoring/homemonitoring/util.py
+++ homemonitoring/homemonitoring/util.py
@@ -1,7 +1,31 @@
 import datetime
+import pytz
+
+import pandas as pd
+import numpy as np
 
 import logging
 import logging.config
+
+
+def get_date_ranges(time_zone, start_datetime, end_datetime=None):
+    tz = pytz.timezone(time_zone)
+    if end_datetime is None:
+        end_datetime = datetime.datetime.now(tz).replace(microsecond=0)
+    else:
+        end_datetime = tz.localize(end_datetime)
+
+    # round to next 15 mins
+    start_datetime = tz.localize(start_datetime)
+    start_datetime += datetime.timedelta(minutes=15 - start_datetime.minute % 15)
+    start_datetime -= datetime.timedelta(seconds=start_datetime.second)
+
+    end_month_dates = pd.date_range(
+        start_datetime, end_datetime, freq='M', normalize=True
+    ).to_pydatetime()
+    end_dates = np.append(end_month_dates, end_datetime)
+    start_dates = np.append(start_datetime, end_dates+datetime.timedelta(days=1))
+    return start_dates, end_dates
 
 
 def get_latest_timestamp_influxdb(measurement_name, ifclient, default):
