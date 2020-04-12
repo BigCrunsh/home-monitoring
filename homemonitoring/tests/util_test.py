ac2be42509b3e8d9c1210b39c823d76cfc82236b
Christoph Sawade <christoph@sawade.me>
2020-04-12 22:01:44 +0200

    Convert timezone if different instead of error
    Make date timezone specific it allow us to correct if different. This is
    necessary because we get utc time stamps from influx and local time
    stamps from now and solaredge interface.

--- homemonitoring/tests/util_test.py
+++ homemonitoring/tests/util_test.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 
 import numpy as np
 from unittest import TestCase
@@ -9,21 +10,24 @@ from homemonitoring.util import get_date_ranges
 class TestUtils(TestCase):
 
     def test_get_date_ranges(self):
+        time_zone = 'Europe/Berlin'
+        tz = pytz.timezone(time_zone)
+
         # start and end in same quarter of hour
-        start_datetime = datetime.datetime(2020, 4, 11, 11, 58, 10, 20)
-        end_datetime = datetime.datetime(2020, 4, 11, 11, 59, 10, 20)
+        start_datetime = datetime.datetime(2020, 4, 11, 11, 58, 10, 20, tzinfo=tz)
+        end_datetime = datetime.datetime(2020, 4, 11, 11, 59, 10, 20, tzinfo=tz)
         s, e = get_date_ranges('Europe/Berlin', start_datetime, start_datetime)
         np.testing.assert_equal(s, np.array([]))
         np.testing.assert_equal(e, np.array([]))
 
         # end is new quarter
-        end_datetime = datetime.datetime(2020, 4, 11, 12, 0, 0, 0)
+        end_datetime = datetime.datetime(2020, 4, 11, 12, 0, 0, 0, tzinfo=tz)
         s, e = get_date_ranges('Europe/Berlin', start_datetime, end_datetime)
-        np.testing.assert_equal(s, np.array([end_datetime]))
-        np.testing.assert_equal(e, np.array([end_datetime]))
+        np.testing.assert_equal(s, np.array([end_datetime.replace(tzinfo=None)]))
+        np.testing.assert_equal(e, np.array([end_datetime.replace(tzinfo=None)]))
 
         # dist between start and end is more than a month (has to be split)
-        end_datetime = datetime.datetime(2020, 5, 11, 12, 0, 0, 0)
+        end_datetime = datetime.datetime(2020, 5, 11, 12, 0, 0, 0, tzinfo=tz)
         s, e = get_date_ranges('Europe/Berlin', start_datetime, end_datetime)
         np.testing.assert_equal(s, np.array([
             datetime.datetime(2020, 4, 11, 12, 0),  # start rounded to full quarter
@@ -32,5 +36,5 @@ class TestUtils(TestCase):
 
         np.testing.assert_equal(e, np.array([
             datetime.datetime(2020, 4, 30, 0, 0),  # last day of month
-            end_datetime
+            end_datetime.replace(tzinfo=None)
         ]))
