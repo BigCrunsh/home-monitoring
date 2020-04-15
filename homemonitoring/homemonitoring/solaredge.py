383ac7f9182a571574e3abfc37a17750e1710afd
Christoph Sawade <christoph@sawade.me>
2020-04-15 20:24:52 +0200

    Add and enforce docstrings

--- homemonitoring/homemonitoring/solaredge.py
+++ homemonitoring/homemonitoring/solaredge.py
@@ -1,3 +1,5 @@
+"""The module contains a wrapper of the Solaredge API with additional functionality."""
+
 import solaredge
 
 import datetime
@@ -8,21 +10,59 @@ import numpy as np
 
 
 class Solaredge(solaredge.Solaredge):
+    """Solaredge is a wrapper around the Solaredge API with additional functionality.
+
+    The Solaredge API is queried with date ranges. Date ranges are relative to the location
+    of the photovoltaic system. The wrapper sets reasonable default values, normalizes dates
+    to be compliant with the API call, and splits the call by month ranges.
+
+    Args:
+        site_token(string): api key
+    """
 
     def __init__(self, *args, **kwargs):
         super(Solaredge, self).__init__(*args, **kwargs)
         self.meta = None
 
     def get_meta(self):
+        """Returns solaredge meta data.
+
+        Queries list end point of API. The call is cached it will return
+        the same result when called twice.
+
+        Returns:
+            dict: meta data
+        """
         if self.meta is None:
             self.meta = self.get_list()['sites']['site'][0]
         return self.meta
 
     def get_site_id(self):
+        """Returns solaredge site id.
+
+        Queries list end point of API. The call is cached it will return
+        the same result when called twice.
+
+        Returns:
+            int: site id
+        """
         return self.get_meta()['id']
 
     @staticmethod
     def _get_date_ranges(start_datetime, end_datetime):
+        """Split date range by months.
+
+        The SolarEdge API is limited to one-month period, i.e., period between endTime and
+        startTime should not exceed one month. This function splits the date range into
+        chunks by months.
+
+        Args:
+            start_datetime (datetime.datetime): start datetime of range
+            end_datetime (datetime.datetime): end datetime of range
+
+        Returns:
+            zip: list of start and end dates by months
+        """
         end_dates = pd.date_range(
             start_datetime, end_datetime, freq='M', normalize=True
         ).to_pydatetime()
@@ -33,6 +73,21 @@ class Solaredge(solaredge.Solaredge):
         return zip(start_dates, end_dates)
 
     def _normalize_date(self, datetime):
+        """Normalizes datetime for SolarEdge API call.
+
+        Normalizes `datetime` to be used in solaredge API, i.e.,
+        - timezone is converted to time zone of system location
+        - time zone info and microseconds are removed (API fails otherwise)
+
+        Args:
+            datetime (datetime.datetime): localized datetime to be normalized
+
+        Raises:
+            AssertionError: if datime misses tzinfo
+
+        Returns:
+            datetime.datetime: datetime normalized for solaredge API call
+        """
         assert datetime.tzinfo is not None
         meta = self.get_meta()
         tz = pytz.timezone(meta['location']['timeZone'])
@@ -56,6 +111,20 @@ class Solaredge(solaredge.Solaredge):
         )
 
     def get_power_details(self, start_time=None, end_time=None):
+        """Returns power details.
+
+        Calls `powerDetails` endpoint of SolarEdge API. The parameters are not
+        limited to one-month period.
+
+        Args:
+            start_time (datetime.datetime): start datetime of range;
+                default: installation date (based on solaredge meta data)
+            end_time (datetime.datetime): end datetime of range
+                default: current timestamp
+
+        Returns:
+            dict: response
+        """
         return [
             super(Solaredge, self).get_power_details(
                 site_id=self.get_site_id(), start_time=s, end_time=e
@@ -67,6 +136,20 @@ class Solaredge(solaredge.Solaredge):
         ]
 
     def get_energy_details(self, start_time=None, end_time=None):
+        """Returns energy details.
+
+        Calls `energyDetails` endpoint of SolarEdge API. The parameters are not
+        limited to one-month period.
+
+        Args:
+            start_time (datetime.datetime): start datetime of range;
+                default: installation date (based on solaredge meta data)
+            end_time (datetime.datetime): end datetime of range
+                default: current timestamp
+
+        Returns:
+            dict: response
+        """
         return [
             super(Solaredge, self).get_energy_details(
                 site_id=self.get_site_id(), start_time=s, end_time=e, time_unit='QUARTER_OF_AN_HOUR'
