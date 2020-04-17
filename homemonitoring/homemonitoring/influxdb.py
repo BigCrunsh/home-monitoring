8cffb81c151e009f2ec7876e2faf7e035a411c31
Christoph Sawade <christoph@sawade.me>
2020-04-17 20:11:46 +0200

    Add docstring to package and influxdb

--- homemonitoring/homemonitoring/influxdb.py
+++ homemonitoring/homemonitoring/influxdb.py
@@ -1,3 +1,5 @@
+"""The module contains extensions of the influxdb client."""
+
 import datetime
 import pytz
 
@@ -5,8 +7,21 @@ import influxdb
 
 
 class InfluxDBClient(influxdb.InfluxDBClient):
+    """InfluxDBClient is a wrapper around the InfluxDB client with additional functionality.
+
+    The InfluxDBClient extends the client by a funtion to get the timestamp of the latest entry
+    of a measurement
+    """
 
     def get_latest_timestamp(self, measurement_name):
+        """Returns timestamp of a latest measurement.
+
+        args:
+            measurement_name (string): name of the measurement in InfluxDB
+
+        Returns:
+            datetime.datetime: timestamp of latest entry
+        """
         res = self.query(f"SELECT * from {measurement_name} ORDER BY DESC LIMIT 1")
         points = list(res.get_points(measurement=measurement_name))
         print(points)
