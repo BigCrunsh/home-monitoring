ed4238adc1b37545e46d5696177b40665ec521f5
Christoph Sawade <christoph@sawade.me>
2020-04-18 14:50:37 +0200

    Fix test
    Changed class to handle list of responses.

--- homemonitoring/tests/response_mappers_test.py
+++ homemonitoring/tests/response_mappers_test.py
@@ -31,7 +31,7 @@ class TestSolarEdgeResponseMapper(TestCase):
                 ]
             }
         }
-        got = SolarEdgeResponseMapper.to_influxdb_point(
+        got = SolarEdgeResponseMapper._to_influxdb_point(
             response_json, time_zone=self.time_zone, measurement_name=self.measurement_name
         )
         self.assertListEqual(got, [])
@@ -54,6 +54,55 @@ class TestSolarEdgeResponseMapper(TestCase):
                 ]
             }
         }
+        got = SolarEdgeResponseMapper._to_influxdb_point(
+            response_json, time_zone=self.time_zone, measurement_name=self.measurement_name
+        )
+        expected = [
+            {
+                'measurement': self.measurement_name,
+                'time': datetime.datetime(2020, 4, 14, 7, 30),
+                'fields': {'Consumption': 316.1671, 'Purchased': 0.0},
+            },
+            {
+                'measurement': self.measurement_name,
+                'time': datetime.datetime(2020, 4, 14, 7, 45),
+                'fields': {'Consumption': 296.30615, 'Purchased': 0.0},
+            }
+        ]
+        self.assertListEqual(got, expected)
+
+    def test_to_influxdb_point_multiple_responses(self):
+        """Checks conversion to influxdb for regular response."""
+        response_json = [
+            {
+                'powerDetails': {
+                    'timeUnit': 'QUARTER_OF_AN_HOUR',
+                    'unit': 'W',
+                    'meters': [
+                        {'type': 'Consumption', 'values': [
+                            {'date': '2020-04-14 09:30:00', 'value': 316.1671},
+                        ]},
+                        {'type': 'Purchased', 'values': [
+                            {'date': '2020-04-14 09:30:00', 'value': 0.0},
+                        ]}
+                    ]
+                }
+            },
+            {
+                'powerDetails': {
+                    'timeUnit': 'QUARTER_OF_AN_HOUR',
+                    'unit': 'W',
+                    'meters': [
+                        {'type': 'Consumption', 'values': [
+                            {'date': '2020-04-14 09:45:00', 'value': 296.30615}
+                        ]},
+                        {'type': 'Purchased', 'values': [
+                            {'date': '2020-04-14 09:45:00', 'value': 0.0}
+                        ]}
+                    ]
+                }
+            },
+        ]
         got = SolarEdgeResponseMapper.to_influxdb_point(
             response_json, time_zone=self.time_zone, measurement_name=self.measurement_name
         )
