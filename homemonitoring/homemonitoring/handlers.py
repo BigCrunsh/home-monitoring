de7290061f51f4a77a3d8fc4fdb67ad8302884a2
Christoph Sawade <christoph@sawade.me>
2020-04-18 09:38:21 +0200

    Simplified tankerkoenig handlers

--- homemonitoring/homemonitoring/handlers.py
+++ homemonitoring/homemonitoring/handlers.py
@@ -1,27 +1,39 @@
+"""The module contains a handler to call the TankerKoenig API."""
+
 import requests
 
 
-class HomeMonitoringHandler(object):
+class TankerKoenigAPI:
+    """TankerKoenigAPI returns gas station prices.
 
-    def __init__(self, api_key, url):
-        self.url = url
-        self.api_key = api_key
+    TankerKoenigAPI queries the API to return gas station prices.
 
+    Args:
+        site_token(string): api key
+    """
 
-class TankerKoenigHandler(object):
     DEFAULT_URL = 'https://creativecommons.tankerkoenig.de'
     DEFAULT_STATION_IDS = {
         "51d4b477-a095-1aa0-e100-80009459e03a": "Jet",
         "005056ba-7cb6-1ed2-bceb-8e5fec1a0d35": "Star"
     }
 
-    def __init__(self, api_key, url=DEFAULT_URL):
-        self.url = url
+    def __init__(self, api_key):
         self.api_key = api_key
 
     def get_prices(self, station_ids=DEFAULT_STATION_IDS.keys()):
+        """Returns gas prices for given station station ids.
+
+        Queries the Tankerkoenig API to return current gas station prices for given station ids.
+
+        Args:
+            station_ids(list): list of station ids
+
+        Returns:
+            dict: response
+        """
         response = requests.get(
-            f'{self.url}/json/prices.php?ids={",".join(station_ids)}&apikey={self.api_key}'
+            f'{self.DEFAULT_URL}/json/prices.php?ids={",".join(station_ids)}&apikey={self.api_key}'
         )
         response.raise_for_status()
         return response.json()
