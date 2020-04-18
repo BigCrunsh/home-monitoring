b03a5e05d450cfb1bf0733beca47738c0472d096
Christoph Sawade <christoph@sawade.me>
2020-04-18 14:26:22 +0200

    Add docstring to util

--- homemonitoring/homemonitoring/util.py
+++ homemonitoring/homemonitoring/util.py
@@ -1,8 +1,19 @@
+"""The module contains some commom util functiosn."""
+
+
 import logging
 import logging.config
 
 
 class LoggerConfig(object):
+    """LoggerConfig allows to configure logging consistently for the package.
+
+    Example:
+        LoggerConfig.set_verbose(True)
+        logger = LoggerConfig.get_logger(__name__)
+        logger.info('test')
+    """
+
     VERBOSE = False
 
     DEFAULT_LOG_DICT = {
@@ -34,10 +45,25 @@ class LoggerConfig(object):
 
     @classmethod
     def set_verbose(cls, verbose):
+        """Sets logging verbosity level.
+
+        Args:
+            verbose (boolean): verbose or not.
+        """
         cls.VERBOSE = verbose
 
     @classmethod
     def get_logger(cls, name):
+        """Returns logger object.
+
+        Returns logger with name.
+
+        Args:
+            name (string): logger name
+
+        Returns:
+            logging.Logger: Logger object
+        """
         logging.config.dictConfig(cls.DEFAULT_LOG_DICT)
         logger = logging.getLogger(name)
         logger.setLevel(logging.DEBUG if cls.VERBOSE else logging.INFO)
