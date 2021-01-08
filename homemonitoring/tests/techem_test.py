a2db618874b02e06212842b99f6efcb957a2b1de
Christoph Sawade <christoph@sawade.me>
2021-01-08 11:42:41 +0100

    Add functions to extract consumption information

--- homemonitoring/tests/techem_test.py
+++ homemonitoring/tests/techem_test.py
@@ -9,8 +9,11 @@ class TestTechemDecoder(TestCase):
     """TestTechemDecoder contains the test cases for the TechenDecoder class."""
 
     def setUp(self):
-        """Sets common params for each test function."""
+        """Sets use case to test."""
         self.id = 53012345
+        self.consumption_before_cutoff = 38524
+        self.consumption_since_cutoff = 287
+        self.total_consumption = 38811
         self.decoder = TechemDecoder('b36446850452301534543CDF7A1009F297C9600881F010080F391ACB2A45C76AA24655A05E5C928932C028921917C0A79E24F460585C59A7DE245F86791B00C')  # noqa
 
     def test__get_byte(self):
@@ -26,3 +29,21 @@ class TestTechemDecoder(TestCase):
     def test_get_meter_id(self):
         """Checks private get byte function."""
         self.assertEqual(self.decoder.get_meter_id(), self.id)
+
+    def test_get_consumption_til_last_cutoff(self):
+        """Checks get_consumption_til_last_cutoff."""
+        self.assertEqual(
+            self.decoder.get_consumption_til_last_cutoff(), self.consumption_before_cutoff
+        )
+
+    def test_get_consumption_since_last_cutoff(self):
+        """Checks get_consumption_since_last_cutoff."""
+        self.assertEqual(
+            self.decoder.get_consumption_since_last_cutoff(), self.consumption_since_cutoff
+        )
+
+    def test_get_total_consumption(self):
+        """Checks get_total_consumption."""
+        self.assertEqual(
+            self.decoder.get_total_consumption(), self.total_consumption
+        )
