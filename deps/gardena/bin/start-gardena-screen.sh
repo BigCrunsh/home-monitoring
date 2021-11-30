e18c27ca7befbdb4716a68a542db4fbc4285c0be
Christoph Sawade <christoph@sawade.me>
2021-11-30 20:18:27 +0100

    [gardena] Run metric collection as daemon
    In contrast to the other python scripts that collect metrics whenever
    the script is run, the gardena script is waiting for the events from the
    API. That script was started manually in a screen, but had to be
    restarted whenever the raspberry was down. This commit adds a script
    that can be scheduled to check and if necessary restart the script.

---
+++ deps/gardena/bin/start-gardena-screen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+ 
+usage() {
+  echo "Start screen to run gardena script as daemon."
+  echo
+  echo "Usage: $(basename $0) [-h]"
+}
+
+function log {
+    echo "[$(basename $0): $(date --rfc-3339=seconds)]: $*"
+}
+
+# global settings 
+SOCKNAME=gardena.collection
+
+while getopts 'h' opt
+do
+  case "${opt}" in
+    h) usage && exit 0;;
+  esac
+done
+
+if [ -z ${GARDENA_APPLICATION_ID} ]; then 
+    log "GARDENA_APPLICATION_ID is not set"
+    exit 1;
+fi
+
+if [ "$(screen -list|grep $SOCKNAME|grep Detached)" ]; then
+    log "screen exists"
+else
+    log "start screen"
+    screen -dmS $SOCKNAME bash -c "$(pwd)/../../../homemonitoring/collect_data_gardena.py"
+    log "done"
+fi
