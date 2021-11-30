3e35615f1cd779d85cd2b9b842a947ff560882eb
Christoph Sawade <christoph@sawade.me>
2021-11-30 20:11:41 +0100

    Make log line fancy

--- deps/techem/bin/activate-mbus.sh
+++ deps/techem/bin/activate-mbus.sh
@@ -6,6 +6,10 @@ usage() {
   echo "Usage: $(basename $0) [-h]"
 }
 
+function log {
+    echo "[$(basename $0): $(date --rfc-3339=seconds)]: $*"
+}
+
 # global settings 
 SLEEP_TIME=300s
 SOCKNAME=nano_cul
@@ -21,13 +25,13 @@ done
 
 
 if [ "$(screen -list|grep $SOCKNAME|grep Detached)" ]; then
-    echo "screen exists"
+    log "screen exists"
 else
-    echo "start screen"
+    log "start screen"
     screen -dmS $SOCKNAME $PORT $BAUDRATE
 
     sleep $SLEEP_TIME;
     screen -X -S $SOCKNAME quit  # the screen is blocking any other interaction with the serial device; to be investigated
-    echo "set wmode"
+    log "set wmode"
     echo "brt" > $PORT
 fi
