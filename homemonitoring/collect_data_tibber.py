766c76fcef5ff93003747fcfdc1d710c04f24532
Christoph Sawade <christoph@sawade.me>
2025-01-20 09:07:10 +0000

    Update tibber data collection script
    API change required change of calls.

--- homemonitoring/collect_data_tibber.py
+++ homemonitoring/collect_data_tibber.py
@@ -17,9 +17,16 @@ from homemonitoring.influxdb import InfluxDBClient
 async def get_tibber_prices(token, user_agent):
     tibber_connection = tibber.Tibber(token, user_agent=user_agent)
     await tibber_connection.update_info()
+    print(tibber_connection.name)
+
     home = tibber_connection.get_homes()[0]
+    await home.fetch_consumption_data()
     await home.update_info()
-    await home.update_price_info()
+    print(home.address1)
+
+    await home.update_current_price_info()
+    print(home.current_price_info)
+
     await tibber_connection.close_connection()
     return home.current_price_info
 
@@ -37,7 +44,6 @@ def run(args):
 
     logger.info("Query Tibber API")
     response_prices = asyncio.run(get_tibber_prices(token=args.api_token, user_agent=args.user_agent))
-
     prices = TibberResponseMapper.to_influxdb_point(response_prices)
     ifclient.write_points(prices)
     logger.info('done')
@@ -54,7 +60,7 @@ def cfg():
     parser.add_argument('--user-agent', required=False, default="Sawade Homemonitoring", help="User agent string submitted to tibber")  # noqa
     parser.add_argument('--influxdb-host', required=False, default=os.getenv('INFLUXDB_HOST'), help="influx db host")  # noqa
     parser.add_argument('--influxdb-port', required=False, default=os.getenv('INFLUXDB_PORT'), help="influx db port")  # noqa
-    parser.add_argument('--influxdb-user', required=False, default=os.getenv('INFLUXDB_PORT'), help="influx db user")  # noqa
+    parser.add_argument('--influxdb-user', required=False, default=os.getenv('INFLUXDB_USER'), help="influx db user")  # noqa
     parser.add_argument('--influxdb-pass', required=False, default=os.getenv('INFLUXDB_PASS'), help="influx db password")  # noqa
     parser.add_argument('--influxdb-db', required=False, default=os.getenv('INFLUXDB_DB'), help="influx db database")  # noqa
     parser.add_argument('-v', '--verbose', action='store_true')
