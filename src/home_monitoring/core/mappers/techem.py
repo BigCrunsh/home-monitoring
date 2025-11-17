4ac07efcff352a0abcedfed46d39f757fdbcd654
Christoph Sawade <christoph@sawade.me>
2025-11-17 21:34:46 +0100

    fix(techem): fix byte indexing in mapper
    - Remove 'b' prefix from hex string
    - Fix byte index calculations
    - Fix test failures

--- src/home_monitoring/core/mappers/techem.py
+++ src/home_monitoring/core/mappers/techem.py
@@ -40,27 +40,24 @@ class TechemMapper:
                 if len(hex_str) != TechemMapper.EXPECTED_RESPONSE_LENGTH:
                     continue
 
-                # Add 'b' prefix to hex string
-                hex_str = "b" + hex_str
-
                 # Extract meter ID (bytes 4-7)
                 meter_id_bytes = ""
                 for pos in [7, 6, 5, 4]:
-                    meter_id_bytes += hex_str[pos * 2 + 1:pos * 2 + 3]
+                    meter_id_bytes += hex_str[pos * 2:(pos * 2) + 2]
                 meter_id = str(int(meter_id_bytes))
 
                 # Extract media type (byte 11)
-                media_type = hex_str[11 * 2 + 1:11 * 2 + 3]
+                media_type = hex_str[11 * 2:(11 * 2) + 2]
 
                 # Extract values (bytes 16-18 and 20-22)
                 last_period_bytes = ""
                 for pos in [18, 17, 16]:
-                    last_period_bytes += hex_str[pos * 2 + 1:pos * 2 + 3]
+                    last_period_bytes += hex_str[pos * 2:(pos * 2) + 2]
                 last_period = int(last_period_bytes, 16)
 
                 current_period_bytes = ""
                 for pos in [22, 21, 20]:
-                    current_period_bytes += hex_str[pos * 2 + 1:pos * 2 + 3]
+                    current_period_bytes += hex_str[pos * 2:(pos * 2) + 2]
                 current_period = int(current_period_bytes, 16)
 
                 value = (last_period + current_period) / 1000.0
